597b43f5f60ae243f9d192d14c36528535da75c9
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-12-06 20:13:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main()
+{
+    int b=0;
+    char a=0;
+    cin >> a;
+    if (vero_falso(a)== true)
+    {
+        cout << lettera (a)<< endl;
+    }
+    else
+    {
+        cout << "errore"<< endl;
+    }
+    return 0;
 }
